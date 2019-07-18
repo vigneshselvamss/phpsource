9c71a2446f4db8a37d1903e8c313b26df775109e
ashokkumarsundar <34398436+ashokkumarsundar@users.noreply.github.com>
2019-07-18 20:12:07 +0530

    Intial Commit

--- index.php
+++ index.php
@@ -1,8 +1,52 @@
 <?php
-require 'vendor/autoload.php';
+require_once 'users/init.php';
+require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+?>
 
-use Carbon\Carbon;
- 
-$date = Carbon::now();
+<?php
 
-echo "Current Date and Time: ".$date->toDayDateTimeString();
+	if ($user->isLoggedIn()) {?>
+<script type="text/javascript" src="https://unpkg.com/amazon-quicksight-embedding-sdk@1.0.1/dist/quicksight-embedding-js-sdk.min.js"></script>
+<script type="text/javascript">
+        function embedDashboard() {
+            var containerDiv = document.getElementById("dashboardContainer");
+            var params = {
+                url: "<?php echo(exec("python3 /var/www/html/getDashboardURL-xki92.py")); ?>",
+                container: containerDiv,
+                parameters: {
+                    country: 'United States'
+                },
+                height: "1140px",
+                width: "1140px"
+            };
+            var dashboard = QuickSightEmbedding.embedDashboard(params);
+            dashboard.on('error', function() {});
+            dashboard.on('load', function() {});
+            //dashboard.setParameters({country: 'Canada'});
+        }
+    </script>
+<?php } ?>
+
+<div>
+			<?php
+				if($user->isLoggedIn()){?>
+					<div id="dashboardContainer"></div>
+					<script type="text/javascript">embedDashboard();</script>
+				<?php }else{?>
+					<h3>Please <a href="users/login.php">login</a> to view details</h3>
+				<?php }?>
+
+
+</div>
+
+<div id="page-wrapper">
+	<div class="container">
+		
+			<?php languageSwitcher();?>
+		</div>
+	</div>
+
+	<!-- Place any per-page javascript here -->
+
+
+	<?php require_once $abs_us_root . $us_url_root . 'users/includes/html_footer.php'; ?>
